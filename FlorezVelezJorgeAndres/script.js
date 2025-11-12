ed0bd1255429dd642dc81e453e951f87cde7c078
kr1pton28 <cuentasgeorge13@gmail.com>
2025-11-12 16:12:25 -0500

    "Excercise #2 finished"

--- FlorezVelezJorgeAndres/script.js
+++ FlorezVelezJorgeAndres/script.js
@@ -7,3 +7,10 @@ let puntosDuplicados = puntos.map((punto) => punto * 2);
 console.log(puntos);
 console.log(puntosDuplicados);
 
+/*
+    Ejercicio 2: Filtrar Nombres (filter)
+*/
+
+let usuarios = [{id: 1, nombre: "Ana"}, {id: 2, nombre: "Luis"}, {id: 3, nombre: "Carlos"}];
+let nombres = usuarios.map((usuario) => usuario.nombre);
+console.log(nombres);
