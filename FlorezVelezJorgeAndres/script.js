b094d039888b01c93c4f1e0d679bed5da158d9ee
kr1pton28 <cuentasgeorge13@gmail.com>
2025-11-12 16:07:27 -0500

    Exercise #1 finished

---
+++ FlorezVelezJorgeAndres/script.js
@@ -0,0 +1,9 @@
+/*
+    Ejercicio 1: Duplicar Números (map)
+*/
+
+let puntos = [10, 20, 30, 40];
+let puntosDuplicados = puntos.map((punto) => punto * 2);
+console.log(puntos);
+console.log(puntosDuplicados);
+
