a059e326631dcf5b85352a01d0870e4e10177d00
kr1pton28 <cuentasgeorge13@gmail.com>
2025-11-12 16:20:23 -0500

    Exercise #4 finished

--- FlorezVelezJorgeAndres/script.js
+++ FlorezVelezJorgeAndres/script.js
@@ -23,3 +23,12 @@ let notas = [8, 5, 10, 3, 7];
 let aprobadas = notas.filter((nota) => nota >= 6);
 console.log(notas);
 console.log(aprobadas);
+
+/*
+    Ejercicio 4: Productos en Stock (filter con objetos)
+*/
+
+let productos = [{nombre: "Camisa", stock: 10}, {nombre: "Zapatos", stock: 0}, {nombre: "Pantalón", stock: 5}];
+let enStock = productos.filter((productos)=>productos.stock > 0);
+console.log(productos);
+console.log(enStock); 
