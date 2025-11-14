88bcb8ed4835b016c51bbab62c0d7d3c9e15acf2
kr1pton28 <cuentasgeorge13@gmail.com>
2025-11-13 22:33:11 -0500

    Exercise #7 finished

--- FlorezVelezJorgeAndres/script.js
+++ FlorezVelezJorgeAndres/script.js
@@ -49,3 +49,13 @@ console.log("Total de ventas es: "+ totalVentas);
 let grupos = [["Ana", "Luis"], ["Maria", "Juan"], ["Pedro"]];
 let listaPlana = grupos.reduce((acum,grupos)=> acum.concat(grupos), []);
 console.log(listaPlana);    
+
+/*
+    Ejercicio 7: Crear una Copia (slice)
+*/
+
+let original = ["A", "B", "C"];
+let copia = original.slice();
+copia.push("D");
+console.log(original);
+console.log(copia);
